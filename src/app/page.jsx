e9c21a50d76d8411dc4dacec6d286649682e199d
Ellen Dutra <ellendutra@ymail.com>
2024-11-07 19:08:24 -0300

    Atualiza responsividade

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -8,13 +8,12 @@ import LogoDesktop from '@/images/logo-desk.svg';
 
 export default function Home() {
   return (
-      <main className="bg-customBlue md:bg-darkBlue absolute inset-0 z-0 bg-no-repeat bg-contain md:bg-cover bg-[url('/background.svg')] md:bg-[url('/background-desk.svg')]">
-        <div className="flex flex-col md:flex-row md:justify-between md:items-start md:ml-[15%] md:w-1/2 ">
-
-          <section className="mt-[45%] md:mt-[35%] md:w-1/2">
+      <main className="bg-customBlue lg:bg-darkBlue absolute inset-0 z-0 bg-no-repeat bg-contain lg:bg-cover bg-[url('/background.svg')] md:bg-[url('/background-desk.svg')]">
+        <div className="flex flex-col lg:flex-row lg:justify-between lg:items-start lg:ml-[15%] lg:w-[40%] xl:w-1/2 ">
+          <section className="mt-[45%] lg:mt-[35%] lg:w-1/2">
             <Image src={Logo} alt="Logotipo Forfun TOP10" className="lg:hidden" priority/>
             <Image src={LogoDesktop} alt="Logotipo Forfun TOP10" className="hidden lg:block" priority/>
-            <div className="text-center md:text-left px-4 md:px-0 md:pl-5">
+            <div className="text-center lg:text-left px-4 lg:px-0">
               <p className="font-dm text-xl">
                 Crie seu TOP 10 de músicas do Forfun e descubra com qual membro da banda você deu match
               </p>
@@ -22,8 +21,8 @@ export default function Home() {
             </div>
           </section>
 
-          <Image src={Mobile} alt="Imagem mobile" className="filter grayscale mt-auto mb-0 md:hidden" priority />
-          <Image src={Desktop} alt="Imagem desktop" className="hidden md:block md:w-auto md:absolute md:right-0 md:h-[90%] md:object-right md:bottom-0 filter grayscale" priority />
+          <Image src={Mobile} alt="Imagem mobile" className="filter grayscale mt-auto mb-0 lg:hidden" priority />
+          <Image src={Desktop} alt="Imagem desktop" className="hidden lg:block lg:w-3/4 lg:h-3/4 2xl:w-auto lg:absolute lg:right-0 xl:h-[90%] lg:object-right lg:bottom-0 filter grayscale" priority />
         
         </div>
       <BottomGradient />
